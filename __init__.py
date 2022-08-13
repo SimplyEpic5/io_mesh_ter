b7ad72621e90298b8e2ff0a0727bb78c763b40a2
SimplyEpic5 <simplyepic5@gmail.com>
2022-08-13 14:06:01 -0600

    Updating for Blender 2.8+

--- __init__.py
+++ __init__.py
@@ -1,9 +1,23 @@
 bl_info = {
+    "name": "Torque DTS format",
+    "author": "port & bansheerubber & irrelevant.irreverent & Eagle517",
+    "version": (0, 3, 6),
+    "blender": (2, 81, 0),
+    "location": "File > Import-Export",
+    "description": "Import-Export DTS, Import DTS mesh, UV's, "
+                   "materials and textures",
+    "warning": "",
+    "support": 'COMMUNITY',
+    "category": "Import-Export"}
+
+
+
+bl_info = {
     "name": "Torque Terrain (TER) format",
     "author": "port",
-    "version": (0, 0, 1),
-    "blender": (2, 76, 0),
-    "location": "File > Import > Torque Terrain",
+    "version": (0, 0, 2),
+    "blender": (2, 81, 0),
+    "location": "File > Import-Export",
     "description": "Import Torque Terrain (TER) files",
     "warning": "",
     "support": "COMMUNITY",
@@ -22,50 +36,104 @@ from . import import_ter
 from bpy.props import (
         StringProperty,
         BoolProperty,
-        CollectionProperty,
-        EnumProperty,
-        FloatProperty,
         )
 from bpy_extras.io_utils import (
         ImportHelper,
-        ExportHelper,
-        orientation_helper_factory,
-        axis_conversion,
         )
 from bpy.types import (
         Operator,
-        OperatorFileListElement,
         )
 
-class ImportTER(Operator, ImportHelper):
-    bl_idname = "import_mesh.ter"
+class ImportTER(bpy.types.Operator, ImportHelper):
+    """Load a Torque TER File"""
+    bl_idname = "import_mesh.import_ter"
     bl_label = "Import Torque Terrain"
-    bl_options = {"UNDO"}
-
+    bl_options = {"PRESET", "UNDO"}
     filename_ext = ".ter"
 
-    filter_glob = StringProperty(
+    filter_glob: StringProperty(
         default="*.ter",
         options={'HIDDEN'},
     )
+    
+    prompt_tex_path: BoolProperty(
+        name="Select Texture Path",
+        description="Select a seperate texture path?",
+        default=False,
+    )
 
     def execute(self, context):
-        keywords = self.as_keywords(ignore=("filter_glob",
-                                            "split_mode",
-                                            ))
+        # keywords = self.as_keywords(ignore=("filter_glob",
+        #                                     "split_mode",
+        #                                     ))
+        ter_file = self.filepath
+            
+        if self.prompt_tex_path:
+            bpy.ops.import_mesh.choose_ter_tex('INVOKE_DEFAULT', ter_file=ter_file)
+        else:
+            bpy.ops.import_mesh.load_ter('EXEC_DEFAULT', ter_file=ter_file, tex_path=None)
 
-        return import_ter.load(context, **keywords)
+        return {'FINISHED'}
+
+# Select a texture directory
+class TexFileChooser(Operator):
+    bl_idname = "import_mesh.choose_ter_tex"
+    bl_label = "Set Texture Path"
+
+    directory: bpy.props.StringProperty(
+        name="Texture Path",
+        description="Texture Path"
+    )
+    
+    ter_file: bpy.props.StringProperty(options={'HIDDEN'})
+
+    def execute(self, context):
+        ter_file = self.ter_file
+        tex_path = self.directory
+        bpy.ops.import_mesh.load_ter('EXEC_DEFAULT', ter_file=ter_file, tex_path=self.directory)
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+class LoadTER(Operator):
+    """Load a Torque TER File"""
+    bl_idname = "import_mesh.load_ter"
+    bl_label = "Load Torque Terrain From File"
+    bl_options = {"PRESET", "UNDO"}
+
+    ter_file: bpy.props.StringProperty(options={'HIDDEN'})
+    tex_path: bpy.props.StringProperty(options={'HIDDEN'})
+
+    def execute(self, context):
+        # keywords = self.as_keywords(ignore=("filter_glob",
+        #                                     "split_mode",
+        #                                     ))
+        
+        ter_file = self.ter_file
+        tex_path = self.tex_path
+        
+        if ter_file is None or ter_file == "":
+            return {'CANCELLED'}
+        
+        return import_ter.load(context, ter_file, tex_path)
 
 def menu_import(self, context):
     self.layout.operator(ImportTER.bl_idname, text="Torque Terrain (.ter)")
 
 def register():
-    bpy.utils.register_module(__name__)
-    bpy.types.INFO_MT_file_import.append(menu_import)
+    bpy.utils.register_class(ImportTER)
+    bpy.utils.register_class(TexFileChooser)
+    bpy.utils.register_class(LoadTER)
+    bpy.types.TOPBAR_MT_file_import.append(menu_import)
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
-    bpy.types.INFO_MT_file_import.remove(menu_import)
+    bpy.utils.unregister_class(ImportTER)
+    bpy.utils.unregister_class(TexFileChooser)
+    bpy.utils.unregister_class(LoadTER)
+    bpy.types.TOPBAR_MT_file_import.remove(menu_import)
+    
 
 if __name__ == "__main__":
     register()
--- import_ter.py
+++ import_ter.py
@@ -10,170 +10,182 @@ BlockSize = 256
 BlockSize2 = BlockSize * BlockSize
 
 def read(fd, fmt):
-	return unpack(fmt, fd.read(calcsize(fmt)))
+   return unpack(fmt, fd.read(calcsize(fmt)))
 
 def read_str(fd):
-	return fd.read(read(fd, "B")[0]).decode("cp1252")
+   return fd.read(read(fd, "B")[0]).decode("cp1252")
 
 def read_str_32(fd):
-	return fd.read(read(fd, "I")[0]).decode("cp1252")
+   return fd.read(read(fd, "I")[0]).decode("cp1252")
 
 class TerrainFile:
     pass
 
 def root_name(path):
-	return os.path.basename(path).rsplit(".", 1)[0]
+   return os.path.basename(path).rsplit(".", 1)[0]
 
 def read_ter(fd):
-	version, = read(fd, "B")
-	assert version == FileVersion, "unsupported format version"
+   version, = read(fd, "B")
+   print(version)
+   #assert version == FileVersion, "unsupported format version"
 
-	ret = TerrainFile()
+   ret = TerrainFile()
 
-	ret.heightMap = read(fd, str(BlockSize2) + "H")
-	ret.materialFlags = read(fd, str(BlockSize2) + "B")
-	ret.materialNames = [read_str(fd) for k in range(MaterialGroups)]
-	ret.materialAlpha = [None] * MaterialGroups
+   ret.heightMap = read(fd, str(BlockSize2) + "H")
+   ret.materialFlags = read(fd, str(BlockSize2) + "B")
+   ret.materialNames = [read_str(fd) for k in range(MaterialGroups)]
+   ret.materialAlpha = [None] * MaterialGroups
 
-	for k in range(MaterialGroups):
-		if ret.materialNames[k]:
-			ret.materialAlpha[k] = read(fd, str(BlockSize2) + "B")
+   for k in range(MaterialGroups):
+      if ret.materialNames[k]:
+         ret.materialAlpha[k] = read(fd, str(BlockSize2) + "B")
 
-	ret.textureScript = read_str_32(fd)
-	ret.heightfieldScript = read_str_32(fd)
+   ret.textureScript = read_str_32(fd)
+   ret.heightfieldScript = read_str_32(fd)
 
-	return ret
+   return ret
 
 texture_extensions = ("png", "jpg")
 
 def resolve_texture(filepath, name):
-	dirname = os.path.dirname(filepath)
+   dirname = os.path.dirname(filepath)
 
-	while True:
-		texbase = os.path.join(dirname, name)
+   while True:
+      texbase = os.path.join(dirname, name)
 
-		for extension in texture_extensions:
-			texname = texbase + "." + extension
+      for extension in texture_extensions:
+         texname = texbase + "." + extension
 
-			if os.path.isfile(texname):
-				return texname
+         if os.path.isfile(texname):
+            return texname
 
-		if os.path.ismount(dirname):
-			break
+      if os.path.ismount(dirname):
+         break
 
-		prevdir, dirname = dirname, os.path.dirname(dirname)
+      prevdir, dirname = dirname, os.path.dirname(dirname)
 
-		if prevdir == dirname:
-			break
+      if prevdir == dirname:
+         break
 
-def load(context, filepath):
-	with open(filepath, "rb") as fd:
-		ter = read_ter(fd)
+def load(context, filepath, tex_path):
+   with open(filepath, "rb") as fd:
+      ter = read_ter(fd)
 
-	name = root_name(filepath)
-	me = bpy.data.meshes.new(name)
+   name = root_name(filepath)
+   me = bpy.data.meshes.new(name)
 
-	for orig_path in ter.materialNames:
-		if not orig_path:
-			continue
+   for orig_path in ter.materialNames:
+      if not orig_path:
+         continue
 
-		mat_name = root_name(orig_path)
-		mat_file = resolve_texture(filepath, mat_name)
+      texture_path = tex_path or filepath
+      mat_name = root_name(orig_path)
+      mat_file = resolve_texture(texture_path, mat_name)
 
-		mat = bpy.data.materials.new(mat_name)
-		me.materials.append(mat)
+      mat = bpy.data.materials.new(mat_name)
+  
+      mat.use_nodes = True
+      mat.roughness = 1
 
-		if mat_file:
-			try:
-				image = bpy.data.images.load(mat_file)
-			except:
-				print("Cannot load image", mat_file)
-				continue
+      if mat_file:
+         try:
+            image = bpy.data.images.load(mat_file)
+         except:
+            print("Cannot load image", mat_file)
+            continue
 
-			slot = mat.texture_slots.add()
-			slot.texture = bpy.data.textures.new(mat_name, "IMAGE")
-			slot.texture.image = image
-			slot.texture_coords = "ORCO"
-			slot.scale = 32, 32, 32
+         # slot = mat.texture_slots.add()
+         # slot.texture = bpy.data.textures.new(mat_name, "IMAGE")
+         # slot.texture.image = image
+         # slot.texture_coords = "ORCO"
+         # slot.scale = 32, 32, 32
+         
+         bsdf = mat.node_tree.nodes["Principled BSDF"]
+         tex = mat.node_tree.nodes.new("ShaderNodeTexImage")
+         tex.image = image
+         mat.node_tree.links.new(bsdf.inputs["Base Color"], tex.outputs["Color"])
+      
+      me.materials.append(mat)
+         
 
-	# This is a bit of a mess.
-	# Feel free to clean it up :)
+   # This is a bit of a mess.
+   # Feel free to clean it up :)
 
-	# me.vertices.add(BlockSize2)
-	#
-	# for i, vert in enumerate(me.vertices):
-	# 	vert.co = (
-	# 		(i // BlockSize) * 8 / 32,
-	# 		(i %  BlockSize) * 8 / -32,
-	# 		ter.heightMap[i] / 32 / 32
-	# 	)
+   # me.vertices.add(BlockSize2)
+   #
+   # for i, vert in enumerate(me.vertices):
+   # 	vert.co = (
+   # 		(i // BlockSize) * 8 / 32,
+   # 		(i %  BlockSize) * 8 / -32,
+   # 		ter.heightMap[i] / 32 / 32
+   # 	)
 
-	verts = []
+   verts = []
 
-	for x in range(BlockSize + 1):
-		for y in range(BlockSize + 1):
-			i = (y % BlockSize) * BlockSize + (x % BlockSize)
+   for x in range(BlockSize + 1):
+      for y in range(BlockSize + 1):
+         i = (y % BlockSize) * BlockSize + (x % BlockSize)
 
-			verts.append((
-				x * 8 - 1024,
-				y * 8 - 1024,
-				ter.heightMap[i] / 32
-			))
+         verts.append((
+            x * 8 - 1024,
+            y * 8 - 1024,
+            ter.heightMap[i] / 32
+         ))
 
-	faces = []
+   faces = []
 
-	for x in range(BlockSize):
-		for y in range(BlockSize):
-			vert0 = y * 257 + x
-			vert1 = y * 257 + x + 1
-			vert2 = y * 257 + x + 257
-			vert3 = y * 257 + x + 258
+   for x in range(BlockSize):
+      for y in range(BlockSize):
+         vert0 = y * 257 + x
+         vert1 = y * 257 + x + 1
+         vert2 = y * 257 + x + 257
+         vert3 = y * 257 + x + 258
 
-			if x % 2 == y % 2:
-				faces.append(((vert0, vert2, vert3), (x, y)))
-				faces.append(((vert3, vert1, vert0), (x, y)))
-			else:
-				faces.append(((vert0, vert2, vert1), (x, y)))
-				faces.append(((vert3, vert1, vert2), (x, y)))
+         if x % 2 == y % 2:
+            faces.append(((vert0, vert2, vert3), (x, y)))
+            faces.append(((vert3, vert1, vert0), (x, y)))
+         else:
+            faces.append(((vert0, vert2, vert1), (x, y)))
+            faces.append(((vert3, vert1, vert2), (x, y)))
 
-	me.vertices.add(len(verts))
-	me.vertices.foreach_set("co", unpack_list(verts))
+   me.vertices.add(len(verts))
+   me.vertices.foreach_set("co", unpack_list(verts))
 
-	me.polygons.add(len(faces))
-	me.loops.add(len(faces) * 3)
+   me.polygons.add(len(faces))
+   me.loops.add(len(faces) * 3)
 
-	# me.uv_textures.new()
-	# uvs = me.uv_layers[0]
+   # me.uv_textures.new()
+   # uvs = me.uv_layers[0]
 
-	for i, ((verts, (gx, gy)), poly) in enumerate(zip(faces, me.polygons)):
-		poly.use_smooth = True
-		poly.loop_total = 3
-		poly.loop_start = i * 3
+   for i, ((verts, (gx, gy)), poly) in enumerate(zip(faces, me.polygons)):
+      poly.use_smooth = True
+      poly.loop_total = 3
+      poly.loop_start = i * 3
 
-		# For now, just solidly fill in the material with the highest alpha
-		best_mat_index = None
-		best_mat_alpha = None
-		grid_idx = gx * BlockSize + gy # ... this is the wrong way around?
+      # For now, just solidly fill in the material with the highest alpha
+      best_mat_index = None
+      best_mat_alpha = None
+      grid_idx = gx * BlockSize + gy # ... this is the wrong way around?
 
-		for mat_index in range(MaterialGroups):
-			if ter.materialAlpha[mat_index]:
-				alpha = ter.materialAlpha[mat_index][grid_idx]
+      for mat_index in range(MaterialGroups):
+         if ter.materialAlpha[mat_index]:
+            alpha = ter.materialAlpha[mat_index][grid_idx]
 
-				if best_mat_index == None or alpha > best_mat_alpha:
-					best_mat_index = mat_index
-					best_mat_alpha = alpha
+            if best_mat_index == None or alpha > best_mat_alpha:
+               best_mat_index = mat_index
+               best_mat_alpha = alpha
 
-		if best_mat_index is not None:
-			poly.material_index = best_mat_index
+      if best_mat_index is not None:
+         poly.material_index = best_mat_index
 
-		for j, index in zip(poly.loop_indices, verts):
-			me.loops[j].vertex_index = index
-			# uvs.data[j].uv = ()
+      for j, index in zip(poly.loop_indices, verts):
+         me.loops[j].vertex_index = index
+         # uvs.data[j].uv = ()
 
-	me.validate()
-	me.update()
+   me.validate()
+   me.update()
 
-	ob = bpy.data.objects.new(name, me)
-	context.scene.objects.link(ob)
+   ob = bpy.data.objects.new(name, me)
+   context.collection.objects.link(ob)
 
-	return {"FINISHED"}
+   return {"FINISHED"}
